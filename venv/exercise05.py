555e25b8dff77d97c621ad9412e688edfbb2c461
uzunovavera <uzunovavera@gmail.com>
2020-03-18 13:46:30 +0100

    Exercises 5 and 6.

---
+++ venv/exercise05.py
@@ -0,0 +1,21 @@
+my_name = 'Z A. Shaw'
+my_age = 35 # not a lie
+my_height = 74.00 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+inch = 2.54
+
+print(f"Lets talk about {my_name}.")
+print(f"Hes {my_height} inches tall.")
+print(f"hes {my_weight} pounds heavy.")
+print("Actually thats not too heavy.")
+print(f"Hes got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}.")
+print(round(inch))
+print(inch * my_height)
