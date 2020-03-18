ac6eb37bcba2f08826645f6764b79bad267bc8ae
uzunovavera <uzunovavera@gmail.com>
2020-03-18 14:30:22 +0100

    Exercises 7, 8, 9 and 10 from the book

---
+++ venv/exercise09.py
@@ -0,0 +1,14 @@
+# Heres some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+monts = "\nJan\nFeb\nMAr\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months :", monts)
+
+print("""
+Theres something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
