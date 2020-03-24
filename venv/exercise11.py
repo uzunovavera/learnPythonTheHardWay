f58c77170d3d8358f620b650d5b5c0f1c3f6a2d6
Vera Uzunova <uzunovavera@gmail.com>
2020-03-24 12:29:39 +0100

    Exercises 11 and 12 from the book

---
+++ venv/exercise11.py
@@ -0,0 +1,17 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+heigh = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you are {age} old, {heigh} cm tall and {weight} kg heavy.")
+
+#example from the internet
+print('Enter your name:')
+x = input()
+print('Hello, ' + x)
+
+#example 2 from the internet
+y = input('Enter your name:')
+print('Hello, ' + y)
